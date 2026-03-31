d27876a7cf07c7b719c3b4937d7bbe8e1a267cbf
CPFarmaAdmin <jm.alfaro@cpfarma.com>
2026-03-31 13:00:16 +0200

    Fix Supabase URL typo

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://rjzhowhopaesxhozhemu.supabase.co';
+const SUPABASE_URL = 'https://rzjhowhopaesxhozhemu.supabase.co';
 const SUPABASE_ANON = 'sb_publishable_nk9JOLF4UYa0dFFRGqaYVw_NfHMIlIC';
 
 const WEBMAIL_URL = 'https://webmail.cpfarma.es';
